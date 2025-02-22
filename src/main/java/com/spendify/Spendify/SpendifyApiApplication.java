966a894c20f03c5b253c088b84a2bcf870ca2644
muntasir-sit <muntasir@synesisit.info>
2025-02-22 22:41:26 +0600

    Initial Commit

---
+++ src/main/java/com/spendify/Spendify/SpendifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.spendify.Spendify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendifyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spendify/Spendify/SpendifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spendify.Spendify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
